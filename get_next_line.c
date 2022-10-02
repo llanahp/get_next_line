ab02d8b90d0cc1d9d4c3e8e765a56db901ac9c2c
Raúl López Llana <51479560+llanahp@users.noreply.github.com>
2022-10-02 14:22:29 +0200

    Init

---
+++ get_next_line.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralopez- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/24 12:38:07 by ralopez-          #+#    #+#             */
+/*   Updated: 2022/09/24 12:38:10 by ralopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*exchange(char *text, char *cad)
+{
+	char	*text_aux;
+
+	text_aux = ft_strjoin(text, cad);
+	free(text);
+	return (text_aux);
+}
+
+static char	*leer(int fd, char *text)
+{
+	int		bytes_read;
+	char	*cad;
+
+	if (text == NULL)
+		text = ft_calloc(1, 1);
+	cad = (char *)ft_calloc(BUFFER_SIZE +1, sizeof(char));
+	if (!cad)
+		return (NULL);
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, cad, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(cad);
+			return (NULL);
+		}
+		cad[bytes_read] = 0;
+		text = exchange(text, cad);
+		if (ft_strchr(text, '\n'))
+			break ;
+	}
+	free(cad);
+	return (text);
+}
+
+char	*obtain_line(char *text)
+{
+	char	*res;
+	int		i;
+
+	i = 0;
+	if (!text[i])
+		return (NULL);
+	while (text[i] && text[i] != '\n')
+		i++;
+	res = NULL;
+	res = (char *)ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	while (text[i] && text[i] != '\n')
+	{
+		res[i] = text[i];
+		i++;
+	}
+	if (text[i] && text[i] == '\n')
+		res[i] = '\n';
+	return (res);
+}
+
+char	*clean_text(char *text)
+{
+	char	*aux;
+	int		i;
+	int		a;
+
+	i = 0;
+	while (text[i] && text[i] != '\n')
+		i++;
+	if (!text[i])
+	{
+		free(text);
+		return (NULL);
+	}
+	aux = NULL;
+	if (text[i] == '\n')
+		i++;
+	aux = (char *)ft_calloc((ft_strlen(text) - i) + 1, sizeof(char));
+	if (!aux)
+		return (NULL);
+	a = 0;
+	while (text[i])
+		aux[a++] = text[i++];
+	free(text);
+	return (aux);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*text;
+	char		*line;
+
+	line = NULL;
+	if (fd < 0 || read(fd, 0, 0) < 0 || BUFFER_SIZE < 0)
+		return (NULL);
+	text = leer(fd, text);
+	if (!text)
+		return (NULL);
+	line = obtain_line(text);
+	text = clean_text(text);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralopez- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/24 12:43:41 by ralopez-          #+#    #+#             */
+/*   Updated: 2022/09/24 12:43:42 by ralopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+#endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <limits.h>
+# include <sys/uio.h>
+# include <sys/types.h>
+
+
+
+#include <stdio.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(const char *c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+#endif

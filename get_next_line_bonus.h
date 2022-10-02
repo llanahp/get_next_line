78f8757533099a76fe96b711b7c10a8e49aaf53f
Raúl López Llana <51479560+llanahp@users.noreply.github.com>
2022-10-02 15:52:34 +0200

    bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ralopez- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/24 12:43:41 by ralopez-          #+#    #+#             */
-/*   Updated: 2022/09/24 12:43:42 by ralopez-         ###   ########.fr       */
+/*   Created: 2022/10/02 14:33:58 by ralopez-          #+#    #+#             */
+/*   Updated: 2022/10/02 14:34:00 by ralopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
@@ -19,6 +19,10 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
 
 char	*get_next_line(int fd);
 void	*ft_calloc(size_t count, size_t size);
